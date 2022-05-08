a06231b90d2ed4dc077076a9cfac90b39c1b5796
AskarKasimov <kasimovaskar123@gmail.com>
2022-05-08 11:17:56 +0400

    navigation graph + material design

--- app/src/main/java/ru/exceptionteapots/pricetrace/MainActivity.java
+++ app/src/main/java/ru/exceptionteapots/pricetrace/MainActivity.java
@@ -1,7 +1,14 @@
 package ru.exceptionteapots.pricetrace;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+
     }
+
+
 }
